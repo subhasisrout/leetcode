aba013a7509ef559e8ef55e4325ccdc0f6f3c59e
Subhasis Rout <srout2000@yahoo.com>
2021-08-15 18:23:19 +0530

    checkin

--- Leetcode/DiagonalTraverseIILC1424.cs
+++ Leetcode/DiagonalTraverseIILC1424.cs
@@ -6,6 +6,9 @@ using System.Linq;
 // #FirstSolution :: All cells having (rowIndex + colIndex) same can be clubbed together and sorted.
 // #SecondSolution :: Use a queue and traverse until queue is not empty. (Idea taken from Leetcode other submissions)
 // Check the diagram which shows the sequencing of row and column index in the queue
+// #SecondSolution is BFS. Read https://leetcode.com/problems/diagonal-traverse-ii/discuss/597690/Python-Simple-BFS-solution-with-detailed-explanation
+// if (i,j) is the root - leftChild would be (i+1,j) and rightChild would be (i,j+1)
+// Most important point in the above link - Note that nums[i][j] is both the left child of nums[i-1][j] and the right child of nums[i][j-1]. To avoid double counting, we only consider a number's left child when we are at the left-most column (j == 0).
 
 namespace Leetcode
 {
@@ -22,7 +25,7 @@ namespace Leetcode
                 int[] current = queue.Dequeue();
                 result.Add(nums[current[0]][current[1]]);
 
-                //current[1]==0 this is needed because you need to go down ONLY if you are in the FIRST ROW (i.e. top most)
+                //current[1]==0 this is needed to prevent double counting. See link above. Try on a paper and process queue (cell) one at a time.
                 if (current[1] == 0 && nums.Count > current[0] + 1)
                     queue.Enqueue(new int[] { current[0] + 1, 0 });
 
--- Leetcode/DiagonalTraverseLC498.cs
+++ Leetcode/DiagonalTraverseLC498.cs
@@ -2,11 +2,56 @@
 
 // Use the technique explained in AlgoExpert i.e Handling directionUP and directionDOWN.
 
+// FindDiagonalOrderEasy taken from https://leetcode.com/problems/diagonal-traverse/discuss/581868/Easy-Python-NO-DIRECTION-CHECKING
+// very intuitive
+// #RememberPattern
+
 namespace Leetcode
 {
     public class DiagonalTraverseLC498
     {
-        public int[] FindDiagonalOrder(int[][] mat)
+        public int[] FindDiagonalOrderEasy(int[][] mat)
+        {
+            Dictionary<int, List<int>> diagonalMap = new Dictionary<int, List<int>>();
+            //Below loop is just filling Dictionary
+            for (int i = 0; i < mat.Length; i++)
+            {
+                for (int j = 0; j < mat[i].Length; j++)
+                {
+                    if (!diagonalMap.ContainsKey(i + j))
+                        diagonalMap.Add(i + j, new List<int>() { mat[i][j] });
+                    else
+                        diagonalMap[i + j].Add(mat[i][j]);
+                }
+            }
+
+            // Below loop is doing snake pattern using % 2 == 0
+            List<int> result = new List<int>();
+            foreach (var key in diagonalMap.Keys)
+            {
+                if (key % 2 == 0)
+                {
+                    var list = diagonalMap[key];
+                    for (int i = list.Count - 1; i >= 0; i--)
+                    {
+                        result.Add(list[i]);
+                    }
+                }
+                else
+                {
+                    var list = diagonalMap[key];
+                    for (int i = 0; i < list.Count ; i++)
+                    {
+                        result.Add(list[i]);
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+
+
+            public int[] FindDiagonalOrderToughToRemember(int[][] mat)
         {
             int rows = mat.Length;
             int cols = mat[0].Length;
