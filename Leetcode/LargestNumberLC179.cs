9a75ab58e2863280c142f905536fb36b4f3d6947
Subhasis Rout <srout2000@yahoo.com>
2021-11-06 18:52:06 +0530

    checkin

---
+++ Leetcode/LargestNumberLC179.cs
@@ -0,0 +1,81 @@
+﻿namespace Leetcode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+
+    public class LargestNumberLC179
+    {
+        public string LargestNumber(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return "";
+            string[] strNums = new string[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                strNums[i] = Convert.ToString(nums[i]);
+            }
+
+            Array.Sort(strNums, new StringComparer());
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < strNums.Length; i++)
+                sb.Append(strNums[i]);
+
+            // After sorting, if the first num (biggest num) is 0, then it means all the nums are 0.
+            // 00000000000
+            if (strNums[0] == "0") return "0";
+            
+            return sb.ToString();
+
+        }
+
+    }
+
+    public class StringComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            string order1 = x + y;
+            string order2 = y + x;
+            return order2.CompareTo(order1);
+        }
+    }
+
+    // Idea from self. Not working.
+    public class MSBComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            while (x > 0 && y > 0)
+            {
+                int msbX = GetMSB(x);
+                int msbY = GetMSB(y);
+
+                if (msbX > msbY)
+                    return msbY.CompareTo(msbX);
+                else if (msbX < msbY)
+                    return msbX.CompareTo(msbY);
+                else
+                {
+                    x = x / 10;
+                    y = y / 10;
+                }
+            }
+            return x.CompareTo(y);
+
+        }
+
+        private int GetMSB(int num)
+        {
+            int msb = num;
+            while (num > 0)
+            {
+                msb = num;
+                num = num / 10;
+            }
+            return msb;
+        }
+    }
+}
+
---
+++ Leetcode/WordBreakLC139.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+// Excellent intuition from Pepcoding https://www.youtube.com/watch?v=2NaaM_z_Jig
+// #DP dp[i] contains the number of sentences that can be formed.
+// for a given 'i', check if the suffixes are present in the dictionary.
+
+namespace Leetcode
+{
+    public class WordBreakLC139
+    {
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            HashSet<string> set = new HashSet<string>();
+            foreach (var str in wordDict)
+            {
+                set.Add(str);
+            }
+
+            int[] dp = new int[s.Length];
+
+            for (int i = 0; i < dp.Length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    string wordToCheck = s.Substring(j, i - j + 1);
+                    if (set.Contains(wordToCheck))
+                    {
+                        if (j > 0)
+                        {
+                            dp[i] += dp[j - 1];
+                        }
+                        else
+                        {
+                            dp[i] = 1;
+                        }
+                    }
+                }
+            }
+            return dp[dp.Length - 1] > 0;
+        
+        }
+    }
+}
