692e100a1ae78bfcc485149eeee4198d1b006806
Subhasis Rout <srout2000@yahoo.com>
2022-03-09 08:17:15 +0530

    checkin

--- Leetcode/PermutationsLC46.cs
+++ Leetcode/PermutationsLC46.cs
@@ -5,6 +5,18 @@
 // Observe the difference between this and LC78
 // // This is more generic than AE version and can be applied in Permuatation-II as well. Learn this.
 
+// Update 6th March 2022 - Generally speaking this can be done using 2 approach. Approach 1 uses extra space (visited array or hashset). Approach 2 does not that 
+// extra space for tracking visited, rather it uses Swapping.
+// Intuition of Approach 2 - You pass idx to the BackTrack method and inside the BackTrack method, you use 'i' from idx to n-1. BackTrack is called again
+// with (idx + 1). BackTracking is done by Swapping again.
+
+// In approach 1, since you are using extra space - you need not pass idx and always run the loop from 0..(n-1)
+// Inspired from Striver channel - Take U forward - https://www.youtube.com/watch?v=YK78FU5Ffjw , https://www.youtube.com/watch?v=f2ic2Rsc9pU
+
+// Looks like for LC47 - you can only use Approach 1 (extra space - Dictionary instead of HashSet). So learn that. Check PermutationsLC47
+// If you dont want to use extra space then Sorting is required PLUS if (visited[i] || i > 0 && nums[i] == nums[i-1] && !visited[i-1]) continue;
+// Not using extra space is NOT INTUITIVE approach
+
 namespace Leetcode
 {
     public class PermutationsLC46
@@ -16,6 +28,8 @@ namespace Leetcode
             PermuteHelper(nums, result, new List<int>(), visited);
             return result;
         }
+
+        //Approach 1
         private void PermuteHelper(int[] nums, IList<IList<int>> result, IList<int> current, bool[] visited)
         {
             if (current.Count == nums.Length)
@@ -36,5 +50,27 @@ namespace Leetcode
                 // above 5-6 lines are a standard for all backtrackking problems.
             }
         }
+
+        // Approach 2
+        /*
+        private void DFSSwapAndBacktrack(IList<IList<int>> ans,int idx, int[] nums){
+            if (idx == nums.Length){
+                ans.Add(new List<int>(nums));
+                return;
+            }
+        
+            for (int i = idx; i < nums.Length; i++){
+                Swap(i,idx,nums);
+                DFSSwapAndBacktrack(ans,idx+1,nums);
+                Swap(i,idx,nums);
+            }
+        
+        }
+    
+        private void Swap(int idx1, int idx2, int[] arr){
+            int temp = arr[idx1];
+            arr[idx1] = arr[idx2];
+            arr[idx2] = temp;
+        }*/
     }
 }
---
+++ Leetcode/PermutationsLC47.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+// #NESTEDMETHOD
+// Intuitive. Inspired from #Neetcode. https://www.youtube.com/watch?v=qhBVWf0YafA 
+// unlike SORTING + if (visited[i] || i > 0 && nums[i] == nums[i-1] && !visited[i-1]) continue;]
+
+
+namespace Leetcode
+{
+    public class PermutationsLC47
+    {
+        public IList<IList<int>> PermuteUnique(int[] nums)
+        {
+            IList<IList<int>> ans = new List<IList<int>>();
+            IList<int> curr = new List<int>();
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (counts.ContainsKey(nums[i]))
+                    counts[nums[i]] += 1;
+                else
+                    counts.Add(nums[i], 1);
+            }
+
+            //NestedMethod
+            void dfsBackTrack()
+            {
+                if (curr.Count == nums.Length)
+                {
+                    ans.Add(new List<int>(curr));
+                    return;
+                }
+
+                foreach (var kvpair in counts)
+                {
+                    if (kvpair.Value > 0)
+                    {
+                        curr.Add(kvpair.Key);
+                        counts[kvpair.Key] -= 1;
+
+                        dfsBackTrack();
+
+                        counts[kvpair.Key] += 1;
+                        curr.RemoveAt(curr.Count - 1);
+                    }
+                }
+            }
+
+            dfsBackTrack();
+            return ans;
+
+        }
+    }
+}
